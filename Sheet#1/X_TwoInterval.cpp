5e99344674dfcf452ced28f75025c194bc6fc519
hironmoy-ray <hpray35@gmail.com>
2024-10-13 19:34:19 +0600

    Two Interval

--- Sheet#1/X_TwoInterval.cpp
+++ Sheet#1/X_TwoInterval.cpp
@@ -1,35 +1,14 @@
 #include <iostream>
 using namespace std;
 
-//  1 20 15 30 -> 15 20
-//  1 50 25 35 -> 25 35
-// 50 60 35 55 -> 50 55
-// 45 55 1 60 -> 45 55
 int main()
 {
     long long int l1,r1,l2,r2;
     cin>>l1>>r1>>l2>>r2;
-    if(l1<=l2){
-        if(r1<=r2){
-            cout<<l2<<" "<<r1;
-        }
-        else if(r1>=r2){
-            cout<<l2<<" "<<r2;
-        }
-        else{
-            cout<<-1;
-        }
-    }
-    else if(l1>l2){
-        if(r1>=r2){
-            cout<<l1<<" "<<r2;
-        }
-        else if(r1<=r2){
-            cout<<l1<<" "<<r1;
-        }
-        else {
-            cout<<-1;
-        }
+    int intSecStart = max(l1,l2);
+    int intSecEnd = min(r1,r2);
+    if(intSecStart<=intSecEnd){
+        cout<<intSecStart<<" "<<intSecEnd;
     }
     else{
         cout<<-1;
