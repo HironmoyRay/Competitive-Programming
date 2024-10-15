0b8fe0fb6b7a3fec022483c7f3656ed924ddb932
hironmoy-ray <hpray35@gmail.com>
2024-10-15 23:52:29 +0600

    The Last 2 digits

--- Sheet#1/Y_TheLast2Digit.cpp
+++ Sheet#1/Y_TheLast2Digit.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
-    
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    a%=100;
+    b%=100;
+    c%=100;
+    d%=100;
+    int x = a*b*c*d;
+    int y= x%100;
+    if(y<10){
+        cout<<0<<y;
+    }
+    else{
+        cout<<y;
+    }
     return 0;
 }
